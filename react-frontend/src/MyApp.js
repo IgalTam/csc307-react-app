c4cf92af89710ee4999042e05ad856fa13bc86ea
IgalTam <igal.tamarkin@gmail.com>
2022-09-24 19:15:55 -0700

    completed form submission capability

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -5,8 +5,6 @@ import Form from './Form'
 function MyApp() {
     const[characters, setCharacters] = useState([]);
     
-
-
     function removeOneCharacter (index) {
             const updated = characters.filter((character, i) => {
             return i !== index
@@ -14,10 +12,14 @@ function MyApp() {
         setCharacters(updated);
     }
 
+    function updateList(person) {
+        setCharacters([...characters, person]);
+    }
+
     return (
         <div className="container">
           <Table characterData={characters} removeCharacter={removeOneCharacter} />
-          <Form />
+          <Form handleSubmit={updateList} />
         </div>
       )
 
